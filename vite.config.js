662a8b8174bcbe851c2fa7a54926aba2ac4715ba
ImJustNate <you@example.com>
2026-02-19 17:21:33 -0700

    top secret testing lol

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        conditions: resolve(__dirname, "src/conditions.html")
+        conditions: resolve(__dirname, "src/conditions.html"),
+        test: resolve(__dirname, "src/test.html")
       }
     }
   }
